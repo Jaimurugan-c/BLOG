9f9fcacc0851fdc4cc8e2e9aac4f81ae9864b559
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-03-10 16:03:27 +0530

    Routes Completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import About from "./assets/pages/About"
+import Dashbord from "./assets/pages/Dashbord"
+import Signin from "./assets/pages/Signin"
+import Signup from "./assets/pages/Signup"
+import Projects from "./assets/pages/Projects"
+import Home from "./assets/pages/Home"
 
 function App() {
- 
+
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path= "/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/dashbord" element={<Dashbord/>} />
+          <Route path="/Projects" element={<Projects/> }/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
