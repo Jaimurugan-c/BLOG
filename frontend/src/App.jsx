0d7fe4d8fa911fa8108fcd431c3ef66483f42964
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-03-13 16:30:54 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import BlogDetails from "./pages/BlogDetails";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import CreatePost from "./pages/CreatePost";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProctedRoute";
+import EditPost from "./pages/EditPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-
-function App() {
+const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/create-post" element={<ProtectedRoute><CreatePost /></ProtectedRoute>} />
+        <Route path="/edit/:id" element={<ProtectedRoute><EditPost /></ProtectedRoute>} />
       </Routes>
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,26 +1,48 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "../api";
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    axios.get("/posts")
-      .then(response => setPosts(response.data))
-      .catch(error => console.log(error));
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/posts");
+        setPosts(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPosts();
   }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.delete(`/posts/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      setPosts(posts.filter(post => post._id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="container mt-5">
-      <h1 className="text-warning">Blog Posts</h1>
+      <h2>Blog Posts</h2>
       <div className="row">
         {posts.map(post => (
           <div key={post._id} className="col-md-4">
-            <div className="card">
+            <div className="card mb-3">
               <img src={post.image} className="card-img-top" alt={post.title} />
               <div className="card-body">
                 <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">{post.description}</p>
+                <p className="card-text">{post.description.substring(0, 100)}...</p>
+                <Link to={`/blog/${post._id}`} className="btn btn-warning">Read More</Link>
+                <Link to={`/edit/${post._id}`} className="btn btn-primary mx-2">Edit</Link>
+                <button className="btn btn-danger" onClick={() => handleDelete(post._id)}>Delete</button>
               </div>
             </div>
           </div>
